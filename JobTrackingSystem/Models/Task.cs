c74d1ebaef388a5db371b67a1c5f40aa23c715ac
Izigraim <z7635332@gmail.com>
2019-09-29 22:34:41 +0300

    update Task.cs

--- JobTrackingSystem/Models/Task.cs
+++ JobTrackingSystem/Models/Task.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobTrackingSystem.Models;
 
 namespace JobTrackingSystem.Models
 {
     public class Task
     {
+        private User whoGave { get; set; }
+        private User whoTake { get; set; }
+        private string taskName { get; set; }
+        private string status { get; set; }
+        private string dateOfTaking { get; set; }
+        private string dateOfFinishing { get; set; }
     }
 }

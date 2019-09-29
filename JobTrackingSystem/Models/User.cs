f51d1fdd9c755f0c83facc887eafd781aa5111de
Izigraim <z7635332@gmail.com>
2019-09-29 22:35:08 +0300

    create User.cs

---
+++ JobTrackingSystem/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTrackingSystem.Models
+{
+    public class User
+    {
+        private string first_name { get; set; }
+        private string last_name { get; set; }
+        private string nickname { get; set; }
+    }
+}

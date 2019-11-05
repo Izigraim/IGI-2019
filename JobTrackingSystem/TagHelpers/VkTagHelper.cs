23c554a382f31b6696c5a8b06e8c55861f459425
Izigraim <z7635332@gmail.com>
2019-11-05 18:30:22 +0300

    add tagHelpers

---
+++ JobTrackingSystem/TagHelpers/VkTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTrackingSystem.TagHelpers
+{
+    public class VkTagHelper : TagHelper
+    {
+        private const string address = "https://vk.com/izigraim";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", address);
+            output.Content.SetContent("Vkontakte");
+        }
+    }
+    
+    public class SocialsTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            var target = await output.GetChildContentAsync();
+            var content = "<p>Социальные сети: </p>" + target.GetContent();
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
